fed4abc2fc739a16f425e2c1812fb0dc277de752
ADHINARAYANAN S <164605101+ADHINARAYANAN2004@users.noreply.github.com>
2025-06-20 21:53:19 +0530

    Add files via upload

---
+++ Forloop.java
@@ -0,0 +1,14 @@
+package Loop;
+
+/**
+ * Forloop
+ */
+public class Forloop {
+    public static void main(String[] args) {
+        for(int i = 1;i<=10;i++){
+            System.out.println(i);
+        }
+    }
+
+    
+}
---
+++ Whileloop.java
@@ -0,0 +1,16 @@
+package Loop;
+
+/**
+ * Whileloop
+ */
+public class Whileloop {
+    public static void main(String[] args){
+        int i = 1;
+        while (i<=5) {
+            System.out.print(i);
+            i++;
+            
+        }
+
+    }
+}
